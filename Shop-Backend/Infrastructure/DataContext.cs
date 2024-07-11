78b79270494f48535e04f97db40ebeb7b371df70
uduwela96 <puduwel@mitrai.com>
2024-07-11 09:40:51 +0530

    in memory db configurations added

---
+++ Shop-Backend/Infrastructure/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shop_Backend.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring
+        (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "ShopDb");
+        }
+    }
+}
